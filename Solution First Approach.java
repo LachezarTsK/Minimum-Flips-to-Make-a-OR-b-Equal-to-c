f84179013aeac6f09ae6f9271df833a9e1a8d9d6
Lachezar Kutsarov <lkutsarov@gmail.com>
2023-06-08 11:22:31 +0200

    Create Solution First Approach.java

---
+++ Solution First Approach.java
@@ -0,0 +1,22 @@
+
+public class Solution {
+
+    public int minFlips(int a, int b, int c) {
+
+        int minFlips = 0;
+        while (a > 0 || b > 0 || c > 0) {
+            int aBit = a & 1;
+            int bBit = b & 1;
+            int cBit = c & 1;
+
+            if ((aBit | bBit) != cBit) {
+                minFlips += (cBit == 0) ? (aBit + bBit) : 1;
+            }
+
+            a >>= 1;
+            b >>= 1;
+            c >>= 1;
+        }
+        return minFlips;
+    }
+}
