15c1cdd5e6b421b875fa75752754765674cf7930
Lachezar Kutsarov <lkutsarov@gmail.com>
2023-06-08 12:07:00 +0200

    Update Solution Second Approach.cpp

--- Solution Second Approach.cpp
+++ Solution Second Approach.cpp
@@ -18,11 +18,11 @@ public:
 
         //C++20: as of June 2023, C++20 is not supported on leetcode.com
         //thus popcount(unsigned integer), defined in <bit>, can not be run the the website.
-        return popcount(static_cast<uint32_t> ((a & b) & ((a | b) ^ c)))
-                + popcount(static_cast<uint32_t> (((a | b) ^ c)));
+        //return popcount(static_cast<uint32_t> ((a & b) & ((a | b) ^ c)))
+        //       + popcount(static_cast<uint32_t> (((a | b) ^ c)));
 
         //as of June 2023, only __builtin_popcount(integer), GCC compiler, defined in <bits/stdc++.h> 
         //can be run on leetcode.com
-        //return __builtin_popcount((a & b) & ((a | b) ^ c)) + __builtin_popcount((a | b) ^ c);
+        return __builtin_popcount((a & b) & ((a | b) ^ c)) + __builtin_popcount((a | b) ^ c);
     }
 };
